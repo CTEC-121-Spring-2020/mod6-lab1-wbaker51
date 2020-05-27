176f94edf334529c5f082c1ef32b3cd760d08941
Bill Baker <bill@baker88.com>
2020-05-27 12:20:28 -0700

    Wed class demo

--- Template.py
+++ Template.py
@@ -12,6 +12,41 @@ Output: return value and description
 """
 
 def main():
-    # code here
+    '''
+    # definite loop example
+    for i in range(0, 11, 2):
+        print(i)
+    print()
+
+    # same behavior but coded as indefinite or while loop
+    count = 0
+    while(count < 11):
+        print(count)
+        #count = count + 2
+        count += 2
+        #count
+        
+    x = True
+    y = False
+    if(x):
+        if(y):
+            print("both true")
+        else:
+            print("y false")
+    else:
+        print("x false")
+    
+    if x and y:
+        print("both true")
+    else:
+        print("other")
+
+    print()
+    '''
+
+    print(True and False)
+    print(True and True)
+    print(True and False or False)
+    print(False or False or True)
 
 main()    
